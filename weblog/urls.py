25252ce6ca3f7d3a1865282ebf6e40f7c4de9acf
djsaeedkhan <djsaeedkhan@yahoo.com>
2017-10-22 15:22:14 +0330

    using forms.py

--- weblog/urls.py
+++ weblog/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
     url(r'add$', views.add, name='add'),
+    url(r'list$', views.list, name='list'),
+    url(r'$', views.index, name='index'),
 ]
--- weblog/views.py
+++ weblog/views.py
@@ -2,8 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import CommentForm
+
+
 def index(request):
     return render(request,'index.html',context=None)
 
 def add(request):
-    return render(request,'add.html',context=None)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            return HttpResponse('ok')
+    else:
+        form = CommentForm()
+    return render(request, 'add.html', {'form': form})
+
+def list(request):
+    return render(request,'list.html',)
