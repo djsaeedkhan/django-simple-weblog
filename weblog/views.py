efb146f4935fd37c88a4d60615b38fd4ecec7242
djsaeedkhan <djsaeedkhan@yahoo.com>
2017-10-23 13:45:49 +0330

    insert data and alert box

--- weblog/views.py
+++ weblog/views.py
@@ -9,15 +9,22 @@ def index(request):
     return render(request,'index.html',context=None)
 
 def add(request):
+    alert="";
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
             title = request.POST.get('title', '')
             body = request.POST.get('body', '')
-            Post(title=title,body=body).save()
+            resp=Post(title=title,body=body)
+            resp.save()
+            if resp.id != 0 :
+                alert = "ثبت اطلاعات با موفقیت انجام شد"
+            else:
+                alert="متاسفانه ثبت اطلاعات انجام نشد"
+
     else:
         form = PostForm()
-    return render(request, 'add.html', {'form': form})
+    return render(request, 'add.html', {'form': form,'alert':alert})
 
 def list(request):
     return render(request,'list.html',)
