1ad5d2c4db7590a91809115067a77519b9b64cd5
djsaeedkhan <djsaeedkhan@yahoo.com>
2017-10-23 11:54:24 +0330

    insert data in sqlite database

--- weblog/views.py
+++ weblog/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 from .forms import PostForm
-
+from .models import Post
 
 def index(request):
     return render(request,'index.html',context=None)
@@ -12,7 +12,9 @@ def add(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            return HttpResponse('ok')
+            title = request.POST.get('title', '')
+            body = request.POST.get('body', '')
+            Post(title=title,body=body).save()
     else:
         form = PostForm()
     return render(request, 'add.html', {'form': form})
